ac0dea5f13d34d0cb9167320f945abdd114d2b68
Ji Wang <jiwang5@microsoft.com>
2021-07-18 23:31:08 +0000

    hide Microsoft.Management managementGroups UUID

--- src/azure_devtools/scenario_tests/recording_processors.py
+++ src/azure_devtools/scenario_tests/recording_processors.py
@@ -66,6 +66,13 @@ class SubscriptionRecordingProcessor(RecordingProcessor):
                         r'\\/\1\\/{}'.format(self._replacement),
                         retval,
                         flags=re.IGNORECASE)
+
+        # Microsoft.Management managementGroups presents in all api call
+        retval = re.sub('/(managementGroups)/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
+                        r'/\1/{}'.format(self._replacement),
+                        val,
+                        flags=re.IGNORECASE)
+
         return retval
 
 
